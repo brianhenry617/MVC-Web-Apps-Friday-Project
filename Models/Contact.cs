d616e92fa640c49aaccfb7a4e5f3fc1f56c24e3b
Brian Henry <94brianhenry@gmail.com>
2018-04-20 12:14:02 -0700

    Add variables for contact book

--- Models/Contact.cs
+++ Models/Contact.cs
@@ -1,26 +1,67 @@
 using System.Collections.Generic;
+using System.IO;
 using System;
 
 namespace ContactList.Models
 {
   public class Contact
   {
-    private string _description;
-    private static List<Contact> _instances = new Contact<Item> {};
+    private string _name;
+    private string _email;
+    private string _phoneNumber;
+    private string _address;
+    private string _birthDate;
+    private static List<Contact> _instances = new List<Contact> {};
 
-    public Contact (string description)
+    public Contact (string name, string email, string phoneNumber, string address, string _birthDate)
     {
-      _description = description;
+      _name = name;
+      _email = email;
+      _phoneNumber = phoneNumber;
+      _address = address;
+      _birthDate = _birthDate;
     }
-    public string GetDescription()
+    public string GetName()
     {
-      return _description;
+      return _name;
     }
-    public void SetDescription(string newDescription)
+    public void SetName(string newName)
     {
-      _description = newDescription;
+      _name = newName;
     }
-    public static Contact<Contact> GetAll()
+    public string GetEmail()
+    {
+      return _email;
+    }
+    public void SetEmail(string newEmail)
+    {
+      _email = newEmail;
+    }
+    public string GetPhoneNumber()
+    {
+      return _phoneNumber
+    }
+    public void SetPhoneNumber(string newPhoneNumber)
+    {
+      _phoneNumber = newPhoneNumber;
+    }
+    public string GetAddress()
+    {
+      return _address;
+    }
+    public void SetAddress(string newAddress)
+    {
+      _address = newAddress;
+    }
+    public string GetBirthDate()
+    {
+      return _birthDate;
+    }
+    public void SetBirthDate(string newBirthdate)
+    {
+      _birthDate = newBirthDate;
+    }
+    public static Item<Contact> GetAll()
     {
       return _instances;
     }
