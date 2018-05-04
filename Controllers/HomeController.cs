a903fec15fb9c5a8afea08f08f36d132a662278e
Brian Henry <94brianhenry@gmail.com>
2018-05-04 09:02:35 -0700

    Update Index Controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,39 +1,44 @@
+using System;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
-using Home.Models;
+using Contact.Models;
 
-namespace Home.Controllers
+namespace Home.Controller
 {
   public class HomeController : Controller
   {
-
-    [HttpGet("/Home")]
+    [Route("/")]
     public ActionResult Index()
     {
-      List<Contact> allContact = Contact.GetAll();
-      return View(allContact);
-    }
-
-    [HttpGet("/Home/new")]
-    public ActionResult CreateForm()
-    {
       return View();
     }
-    [HttpPost("/Home")]
-    public ActionResult Create()
-    {
-      Contact newContact = new Contact (Request.Form["name"], Request.Form["email"], Request.Form["phoneNumber"], Request.Form["address"], Request.Form["birthDate"]);
-      newContact.Save();
-      List<Contact> allContact = Contact.GetAll();
-      return View("Home", allContact);
-    }
-
-    [HttpPost("/Home/delete")]
-    public ActionResult DeleteAll()
-    {
-      Contact.ClearAll();
-      return View();
-    }
-
   }
 }
+
+//       List<Contact> allContact = Contact.GetAll();
+//       return View(allContact);
+//     }
+//
+//     [HttpGet("/Home/new")]
+//     public ActionResult CreateForm()
+//     {
+//       return View();
+//     }
+//     [HttpPost("/Home")]
+//     public ActionResult Create()
+//     {
+//       Contact newContact = new Contact (Request.Form["name"], Request.Form["email"], Request.Form["phoneNumber"], Request.Form["address"], Request.Form["birthDate"]);
+//       newContact.Save();
+//       List<Contact> allContact = Contact.GetAll();
+//       return View("Home", allContact);
+//     }
+//
+//     [HttpPost("/Home/delete")]
+//     public ActionResult DeleteAll()
+//     {
+//       Contact.ClearAll();
+//       return View();
+//     }
+//
+//   }
+// }
