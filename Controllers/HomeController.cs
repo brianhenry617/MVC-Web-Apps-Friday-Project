d8fa39cbd0afe1707910b224ce3814ebc739dfd7
Brian Henry <94brianhenry@gmail.com>
2018-05-04 09:15:16 -0700

    Remove commented out stuff that does not belong in the document

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -14,31 +14,3 @@ namespace Home.Controller
     }
   }
 }
-
-//       List<Contact> allContact = Contact.GetAll();
-//       return View(allContact);
-//     }
-//
-//     [HttpGet("/Home/new")]
-//     public ActionResult CreateForm()
-//     {
-//       return View();
-//     }
-//     [HttpPost("/Home")]
-//     public ActionResult Create()
-//     {
-//       Contact newContact = new Contact (Request.Form["name"], Request.Form["email"], Request.Form["phoneNumber"], Request.Form["address"], Request.Form["birthDate"]);
-//       newContact.Save();
-//       List<Contact> allContact = Contact.GetAll();
-//       return View("Home", allContact);
-//     }
-//
-//     [HttpPost("/Home/delete")]
-//     public ActionResult DeleteAll()
-//     {
-//       Contact.ClearAll();
-//       return View();
-//     }
-//
-//   }
-// }
