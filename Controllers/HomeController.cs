ddb1861ee5cb15dbe2122c404f6ff45a99b1c78b
Brian Henry <94brianhenry@gmail.com>
2018-05-04 12:06:53 -0700

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,13 +3,35 @@ using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using Contact.Models;
 
-namespace Home.Controller
+namespace Contact.Controller
 {
   public class HomeController : Controller
   {
-    [Route("/")]
+    [HttpGet("/")]
     public ActionResult Index()
     {
+      List <Contact> all Contacts = Contact.GetAll();
+      return View(allContacts);
+    }
+    
+    [HttpGet("/contact/new")]
+    public ActionResult CreateForm()
+    {
+      return View();
+    }
+    
+    [HttpPost("/contact")]
+    public ActionResult Create()
+    {
+      Contact newContact = new Contact(Request.Form["name"], Request.Form["email"], Request.Form["phoneNumber"], Request.Form["address"], Request.Form["birthDate"]);
+      List <Contact> allContacts = Contact.GetAll();
+      return View("Index");
+    }
+    
+    [HttpPost("contact/clear")]
+    public ActionResult Clear()
+    {
+      Contact.ClearAll();
       return View();
     }
   }
